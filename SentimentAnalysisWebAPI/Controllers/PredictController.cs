4c1dc6a862b7ef1698855e1985895d20ce52eed0
ianormy <ianormy@gmail.com>
2020-02-03 20:54:12 +0000

    Add project files.

---
+++ SentimentAnalysisWebAPI/Controllers/PredictController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.ML;
+using SentimentAnalysisWebAPI.DataModels;
+
+namespace SentimentAnalysisWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PredictController : ControllerBase
+    {
+        private readonly PredictionEnginePool<SentimentData, SentimentPrediction> _predictionEnginePool;
+
+        public PredictController(PredictionEnginePool<SentimentData, SentimentPrediction> predictionEnginePool)
+        {
+            _predictionEnginePool = predictionEnginePool;
+        }
+
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] SentimentData input)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            SentimentPrediction prediction = _predictionEnginePool.Predict(modelName: "SentimentAnalysisModel", example: input);
+
+            string sentiment = Convert.ToBoolean(prediction.Prediction) ? "Positive" : "Negative";
+
+            return Ok(sentiment);
+        }
+    }
+}
